ad567ceecdc39dcdd1bda904557bcd4a28224af6
d1gl8 <d1gl8@yandex.ru>
2024-07-17 14:06:50 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Vue 3 start template</h1>
+  <ul>
+    <li>Vite</li>
+    <li>Typescript</li>
+    <li>ESLint</li>
+    <li>Prettier</li>
+    <li>SCSS</li>
+  </ul>
+</template>
+
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 10px;
+}
+</style>
